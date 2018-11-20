93a800394c971a261b8ae9471ab52ba508088a52
jasperezkl <44989279+jasperezkl@users.noreply.github.com>
2018-11-20 10:56:46 +0800

    Update 8FibonacciSequence.java

--- 8FibonacciSequence.java
+++ 8FibonacciSequence.java
@@ -21,7 +21,7 @@ public class FibonacciSequence {
         return 1;
 }
     else{
-        return fibonacci(position-1)+(position-2);
+        return fibonacci(position-1)+fibonacci(position-2);
         }
     }
 }
